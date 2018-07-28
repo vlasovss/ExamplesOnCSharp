960909e949afd08a4ac29c42786ecac1b29e65d4
vlasovss <40613425+vlasovss@users.noreply.github.com>
2018-07-28 21:48:40 +0300

    Пример консольного приложения BankApplication
    Пример взят с https://metanit.com/sharp/tutorial/3.29.php, В примере применяется большая часть основных концепции объектно-ориентированного программирования, рассматриваемых в https://metanit.com/sharp/tutorial/.

---
+++ BankApplication/BankApplication/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using BankLibrary;
+
+using static System.Console;
+
+namespace BankApplication
+{
+    class Program
+    {
+        private static void OpenAccountHandler(object sender, AccountEventArgs e)
+        {
+            WriteLine(e.Message);
+        }
+
+        private static void CloseAccountHandler(object sender, AccountEventArgs e)
+        {
+            WriteLine(e.Message);
+        }
+
+        private static void AddSumHandler(object sender, AccountEventArgs e)
+        {
+            WriteLine(e.Message);
+        }
+
+        private static void WithdrawSumHandler(object sender, AccountEventArgs e)
+        {
+            WriteLine(e.Message);
+            if (e.Sum > 0) { WriteLine("Идем тратить деньги."); }
+        }
+
+        private static void OpenAccount(Bank<Account> bank)
+        {
+            WriteLine("Укажите сумму для создания счета:");
+
+            decimal sum = Convert.ToDecimal(ReadLine());
+            WriteLine("Выберите тип счета: 1. До востребования; 2. Депозит.");
+
+            AccountType accountType;
+            int type = Convert.ToInt32(ReadLine());
+
+            if (type == 2) { accountType = AccountType.Deposit; }
+            else { accountType = AccountType.Ordinary; }
+
+            bank.Open(accountType,
+                sum,
+                AddSumHandler,
+                WithdrawSumHandler,
+                (o, e) => WriteLine(e.Message),
+                CloseAccountHandler,
+                OpenAccountHandler);
+        }
+
+        private static void CloseAccount(Bank<Account> bank)
+        {
+            WriteLine("Введите Id счета, который нужно закрыть:");
+            int id = Convert.ToInt32(ReadLine());
+
+            bank.Close(id);
+        }
+
+        private static void Put(Bank<Account> bank)
+        {
+            WriteLine("Укажите сумма, чтобы положить на счета");
+            decimal sum = Convert.ToDecimal(ReadLine());
+
+            WriteLine("Введите Id:");
+            int id = Convert.ToInt32(ReadLine());
+
+            bank.Put(sum, id);
+        }
+
+        private static void Withdraw(Bank<Account> bank)
+        {
+            WriteLine("Укажите сумма для вывода со счета:");
+            decimal sum = Convert.ToDecimal(ReadLine());
+
+            WriteLine("Введите Id:");
+            int id = Convert.ToInt32(ReadLine());
+
+            bank.Withdraw(sum, id);            
+        }
+
+        private static void Main(string[] args)
+        {
+            Bank<Account> Unitbank = new Bank<Account>("ЮнитБанк");
+
+            bool alive = true;
+            while (alive)
+            {
+                ConsoleColor color = ForegroundColor;
+                ForegroundColor = ConsoleColor.DarkGreen;
+                WriteLine("1. Открыть счет. \t 2. Вывести средства. \t 3. Добавить на счет.");
+                WriteLine("4. Закрыть счет. \t 5. Пропустить день. \t 6. Выйти из программы.");
+                WriteLine("Введите номер пункта: ");
+                ForegroundColor = color;
+                try
+                {
+                    int command = Convert.ToInt32(ReadLine());
+
+                    switch (command)
+                    {
+                        case 1:
+                            {
+                                OpenAccount(Unitbank);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Withdraw(Unitbank);
+                                break;
+                            }
+                        case 3:
+                            {
+                                Put(Unitbank);
+                                break;
+                            }
+                        case 4:
+                            {
+                                CloseAccount(Unitbank);
+                                break;
+                            }
+                        case 5:
+                            {
+                                break;
+                            }
+                        case 6:
+                            {
+                                alive = false;
+                                continue;
+                            }
+                    }
+                    Unitbank.CalculatePercentage();
+                }
+                catch (Exception exeption)
+                {
+                    color = ForegroundColor;
+                    ForegroundColor = ConsoleColor.Red;
+                    WriteLine(exeption.Message);
+                    ForegroundColor = color;
+                }
+            }
+        }
+    }
+}
---
+++ BankApplication/BankLibrary/Account.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankLibrary
+{
+    public abstract class Account : IAccount
+    {
+        protected internal event AccountStateHandler Withdrawed;
+        protected internal event AccountStateHandler Added;
+        protected internal event AccountStateHandler Opened;
+        protected internal event AccountStateHandler Closed;
+        protected internal event AccountStateHandler Calculated;
+
+        protected int _id;
+        static int counter = 0;
+
+        protected decimal _sum;
+        protected int _percentage;
+
+        protected int _days = 0;
+
+        public decimal CurrentSum { get => _sum; }
+        public int Percentage { get => _percentage; }
+        public int Id { get => _id; }
+
+        protected Account(decimal sum, int percentage)
+        {
+            this._sum = sum;
+            this._percentage = percentage;
+            this._id = ++counter;
+        }
+
+        private void CallEvent(AccountEventArgs e, AccountStateHandler handler)
+        {
+            if ((handler != null) && (e != null)) { handler(this, e); }
+        }
+
+        protected virtual void OnOpened(AccountEventArgs e) => CallEvent(e, Opened);
+        protected virtual void OnWithdrawed(AccountEventArgs e) => CallEvent(e, Withdrawed);
+        protected virtual void OnAdded(AccountEventArgs e) => CallEvent(e, Added);
+        protected virtual void OnClosed(AccountEventArgs e) => CallEvent(e, Closed);
+        protected virtual void OnCalculated(AccountEventArgs e) => CallEvent(e, Calculated);
+
+        public virtual void Put(decimal sum)
+        {
+            this._sum += sum;
+            OnAdded(new AccountEventArgs($"На счет поступило {sum}", sum));
+        }
+
+        public virtual decimal Withdraw(decimal sum)
+        {
+            decimal result = 0;
+            if (this._sum <= sum)
+            {
+                this._sum -= sum;
+                result = sum;
+                OnWithdrawed(new AccountEventArgs($"Сумма {sum} снята со счета {this._id}", sum));
+            }
+            else
+            {
+                OnWithdrawed(new AccountEventArgs($"Недостаточно денег на счете {this._id}", 0));
+            }
+
+            return result;
+        }
+
+        protected internal virtual void Open()
+        {
+            OnOpened(new AccountEventArgs($"Открыт новый депозитный счет! Id счета {this._id}", this._sum));
+        }
+
+        protected internal virtual void Close()
+        {
+            OnClosed(new AccountEventArgs($"Cчет {this._id} закрыт. Итогоавя сумма: {CurrentSum}", CurrentSum));
+        }
+
+        protected internal void IncrementDays() => this._days++; 
+
+        protected internal virtual void Calculate()
+        {
+            decimal increment = this._sum * _percentage / 100;
+            this._sum += increment;
+            OnCalculated(new AccountEventArgs($"Начислены проценты в размере: {increment}", increment));
+        }
+    }
+}
